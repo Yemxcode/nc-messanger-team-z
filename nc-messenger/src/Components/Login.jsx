fbdb6221b900413370e9ceb9a405053b2c505916
julRus <julia_russell4@outlook.com>
2019-12-18 12:06:02 +0000

    created login page with sudo functionallity

--- nc-messenger/src/Components/Login.jsx
+++ nc-messenger/src/Components/Login.jsx
@@ -1,7 +1,62 @@
 import React from "react";
 
-const Login = props => {
-  return <>OH MY GAWSH!</>;
-};
+export default class Login extends React.Component {
+  state = {
+    username: "",
+    avatar: ""
+  };
 
-export default Login;
+  render() {
+    return (
+      <>
+        <form
+          onSubmit={e => {
+            this.handleSubmit(e);
+          }}
+        >
+          <label>
+            Username
+            <input
+              type="text"
+              placeholder="username"
+              value={this.state.username}
+              id="username"
+              className="loginFormItem"
+              onChange={e => {
+                this.handleChange(e);
+              }}
+            />
+          </label>
+          <br />
+          <label>
+            Avatar URL
+            <input
+              type="text"
+              placeholder="avatar URL"
+              value={this.state.avatar}
+              id="avatar"
+              className="loginFormItem"
+              onChange={e => {
+                this.handleChange(e);
+              }}
+            />
+            <br />
+          </label>
+          <button>ENTER</button>
+        </form>
+      </>
+    );
+  }
+
+  handleChange(e) {
+    const { id, value } = e.target;
+    this.state({ [id]: value });
+  }
+
+  handleSubmit(e) {
+    const { username, avatar } = this.state;
+    e.preventDefualt();
+    this.props.myFunc(username, avatar);
+    this.setState({ username: "", avatar: "" });
+  }
+}
